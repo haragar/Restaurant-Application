633e199c96c149f1e57cc07421ae22398cc41011
unknown <agarwalh614@gmail.com>
2024-03-12 10:05:17 +0530

    Restuarant-Application-React

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">HARSHU</div>
+          <div className="right">
+            <p>LNMIIT, Jamdoli</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Harsh</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
